b52e0a5559c1c02463f61731dfcb1f893d261090
GrrrD <gerapl27@gmail.com>
2021-10-22 11:20:32 -0400

    change events to appointments

--- app/components/schedule.js
+++ app/components/schedule.js
@@ -2,16 +2,16 @@ import Component from '@glimmer/component';
 
 export default class ScheduleComponent extends Component {
   get staffMembers() {
-    // console.log(this.args.events);
+    // console.log(this.args.appointments);
     return hardcodedStaffMembers;
   }
 }
 
 const hardcodedStaffMembers = [
-  { name: 'Hardcoded 1', role: 'Provider', events: [{ title: 'Visit' }] },
+  { name: 'Hardcoded 1', role: 'Provider', appointments: [{ title: 'Visit' }] },
   {
     name: 'Hardcoded 2',
     role: 'Provider',
-    events: [{ title: 'New patient visit' }, { title: 'Soreness' }],
+    appointments: [{ title: 'New patient visit' }, { title: 'Soreness' }],
   },
 ];
