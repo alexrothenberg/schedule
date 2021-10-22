034356170084bb1d0646399853c87058a7b9e54e
GrrrD <gerapl27@gmail.com>
2021-10-22 11:01:42 -0400

    change event name to event title

--- app/components/schedule.js
+++ app/components/schedule.js
@@ -2,15 +2,16 @@ import Component from '@glimmer/component';
 
 export default class ScheduleComponent extends Component {
   get staffMembers() {
+    // console.log(this.args.events);
     return hardcodedStaffMembers;
   }
 }
 
 const hardcodedStaffMembers = [
-  { name: 'Hardcoded 1', role: 'Provider', events: [{ name: 'Visit' }] },
+  { name: 'Hardcoded 1', role: 'Provider', events: [{ title: 'Visit' }] },
   {
     name: 'Hardcoded 2',
     role: 'Provider',
-    events: [{ name: 'New patient visit' }, { name: 'Soreness' }],
+    events: [{ title: 'New patient visit' }, { title: 'Soreness' }],
   },
 ];
